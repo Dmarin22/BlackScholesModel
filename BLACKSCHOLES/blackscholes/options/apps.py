ec2db6dd7eb7bd114d75038aaa88ea2f7af97375
maucolon <144188891+maucolon@users.noreply.github.com>
2024-10-17 13:27:34 -0400

    Delete BLACKSCHOLES directory

--- BLACKSCHOLES/blackscholes/options/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class OptionsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "options"
--- BLACKSCHOLES/blackscholes/options/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns= [
-    path('calculate/', views.calculate_option, name='calculate_option')
-]
